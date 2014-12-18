bb293ab7b02f6f64223f84d83c408693c0e17c4b
Frank77GLD <pfmzuidam@msn.com>
2014-12-18 17:31:39 +0100

    Update cm9.
    Update cm9.

--- cm.mk
+++ cm.mk
@@ -1,5 +1,4 @@
 ## Specify phone tech before including full_phone
-#onder wel in org file.
 #$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name. nakijken
@@ -7,15 +6,10 @@ PRODUCT_RELEASE_NAME := m805_892x
 
 # Preload bootanimation. nog proberen.
 TARGET_BOOTANIMATION_PRELOAD := true
-#Hier onder nakijken..
-#TARGET_SCREEN_HEIGHT := 480
-#TARGET_SCREEN_WIDTH := 800
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
 #TARGET_BOOTANIMATION_NAME := horizontal-1024x600
-#TARGET_BOOTANIMATION_NAME := horizontal-1024x600
-#Toevegen 2014?
-#PRODUCT_COPY_FILES += \
-#
-#     vendor/cm/prebuilt/common/bootanimation/???240.zip:system/media/bootanimation.zip
+#LANDSCAPE_RESOLUTION := 800x480?
 
 #nieuw 28 jan
 # Inherit some common CM stuff.
@@ -24,13 +18,24 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 # Inherit some common CM stuff.
 #$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit device configuration
+# Inherit device configuration. uitzoeken welke meest voorkomt cm11. full_m805_892x.mk moet eerst! Die start device_m805_892x.mk
 $(call inherit-product, device/YG/m805_892x/full_m805_892x.mk)
+#$(call inherit-product, device/YG/m805_892x/device_m805_892x.mk)
+
+#test 7dec werkt niet!
+#PRODUCT_COPY_FILES += \
+#device/YG/m805_892x/prebuild/bootanimation.zip:system/media/bootanimation.zip
 
 ## Device identifier. This must come after all inclusions
+#PRODUCT_NAME := full_m805_892x
+PRODUCT_NAME := full_m805_892x_evm
 PRODUCT_DEVICE := m805_892x
-#PRODUCT_NAME := cm_m805_892x oude
-PRODUCT_NAME :=  full_m805_892x
-PRODUCT_BRAND := YG
-PRODUCT_MODEL := m805_892x
+#PRODUCT_NAME := cm_m805_892x
+#PRODUCT_BRAND := YG
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := A777
+#PRODUCT_MODEL := m805_892x
 PRODUCT_MANUFACTURER := YG
+#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=US_epad BUILD_FINGERPRINT=Android/full_m805_892x_evm/m805_892x:4.0.3/A777/eng.zxp.20120426.164102:userdebug/test-keys
+# Release name and versioning
+#PRODUCT_RELEASE_NAME := tf101
