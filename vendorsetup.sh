ec8578a5543cb96c57b0d6143b92daff2e2cd629
Frank77GLD <pfmzuidam@msn.com>
2014-12-18 21:15:59 +0100

    Update vendorsetup.sh
    Some change

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,9 @@ add_lunch_combo device_m805_892x-eng
 add_lunch_combo full_m805_892x-eng
 add_lunch_combo full_m805_892x-user
 add_lunch_combo full_m805_892x-userdebug
-add_lunch_combo device_8048-eng
-add_lunch_combo device_8048-userdebug
-add_lunch_combo full_8048-eng
-add_lunch_combo full_8048-userdebug
+add_lunch_combo full_m805_892x_evm-userdebug
+add_lunch_combo cm_m805_892x-userdebug
+#add_lunch_combo device_8048-eng
+#add_lunch_combo device_8048-userdebug
+#add_lunch_combo full_8048-eng
+#add_lunch_combo full_8048-userdebug
